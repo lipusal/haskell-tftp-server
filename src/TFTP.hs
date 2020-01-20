2b3883adc959975cd9e38bde882c14a6dd31e25a
Juan Li Puma <juanlipuma94@gmail.com>
2020-01-19 22:25:14 -0300

    First working processing and reply

--- src/TFTP.hs
+++ src/TFTP.hs
@@ -2,8 +2,9 @@ module TFTP where
 
 import Data.Word
 import Data.Char
-import Data.Bits (shiftL, (.|.))
-import qualified Data.ByteString
+import Data.Bits (shiftL, shiftR, (.&.), (.|.))
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as C
 import Debug.Trace
 import System.IO
 import System.IO.Error
@@ -13,30 +14,57 @@ blockSize = 512
 data Packet =
     RRQ String String -- opcode 1 + filename + 0 + mode + 0
     | WRQ String String -- opcode 2 + filename + 0 + mode + 0
-    | DATA Word16 Data.ByteString.ByteString -- opcode 3 + block number + data TODO also consider strict/lazy bytestrings
+    | DATA Word16 BS.ByteString -- opcode 3 + block number + data TODO also consider strict/lazy bytestrings
     | DATA2 Word16 [Word8] -- opcode 3 + block number + data TODO also consider strict/lazy bytestrings
     | ACK Word16 -- opcode 4 + block number
     | ERROR Word16 String deriving Show -- opcode 5 + error code + error message + 0
 
-fromByteString :: Data.ByteString.ByteString -> Maybe Packet
+fromByteString :: BS.ByteString -> Maybe Packet
 fromByteString bs = fromOpcode opcode payload
-    where (header,payload) = Data.ByteString.splitAt 2 bs
+    where (header,payload) = BS.splitAt 2 bs
           opcode = byteStringToWord16 header
 
-fromOpcode :: Word16 -> Data.ByteString.ByteString -> Maybe Packet
+fromOpcode :: Word16 -> BS.ByteString -> Maybe Packet
 fromOpcode 1 d = Just (RRQ filename mode)
-    where (filename:mode:_) = map byteStringtoString (Data.ByteString.split 0 d)
+    where (filename:mode:_) = map byteStringtoString (BS.split 0 d)
 fromOpcode 2 d = Just (WRQ filename mode)
-    where (filename:mode:_) = map byteStringtoString (Data.ByteString.split 0 d)
+    where (filename:mode:_) = map byteStringtoString (BS.split 0 d)
 fromOpcode 3 d = Just (DATA blockNum payload)
-    where (header,payload) = Data.ByteString.splitAt 2 d
+    where (header,payload) = BS.splitAt 2 d
           blockNum = byteStringToWord16 header
 fromOpcode 4 d = Just (ACK (byteStringToWord16 d))
 fromOpcode 5 d = Just (ERROR errCode errMsg)
-    where (header,errBytes) = Data.ByteString.splitAt 2 d
+    where (header,errBytes) = BS.splitAt 2 d
           errCode = byteStringToWord16 header
-          errMsg = byteStringtoString (Data.ByteString.init errBytes) -- init to remove the trailing 0
+          errMsg = byteStringtoString (BS.init errBytes) -- init to remove the trailing 0
 fromOpcode x y = trace ("Received opcode=" ++ show x ++ " and data=" ++ byteStringtoString y) Nothing
+
+toByteString :: Packet -> BS.ByteString
+-- TODO check out Data.ByteString.Builder
+toByteString (RRQ filename mode) = BS.concat [
+    word16ToByteString 1,
+    stringToByteString filename,
+    BS.singleton 0,
+    stringToByteString mode,
+    BS.singleton 0]
+toByteString (WRQ filename mode) = BS.concat [
+    word16ToByteString 2,
+    stringToByteString filename,
+    BS.singleton 0,
+    stringToByteString mode,
+    BS.singleton 0]
+toByteString (DATA blocknum payload) = BS.concat [
+    word16ToByteString 3,
+    word16ToByteString blocknum,
+    payload]
+toByteString (ACK blocknum) = BS.concat [
+    word16ToByteString 4,
+    word16ToByteString blocknum]
+toByteString (ERROR errNum errMsg) = BS.concat [
+    word16ToByteString 5,
+    word16ToByteString errNum,
+    stringToByteString errMsg,
+    BS.singleton 0]
 
 
 process :: Packet -> IO ([Packet])
@@ -50,12 +78,12 @@ fileToPackets :: String -> String -> IO ([Packet])
 fileToPackets filename "netascii" = (do
     -- TODO the only thing that changes here is openFile/openBinaryFile. DRY
     handle <- openFile filename ReadMode
-    contents <- Data.ByteString.hGetContents handle
+    contents <- BS.hGetContents handle
     return (mapWithIndex (\dat index -> DATA (fromIntegral (index+1)) dat) (chunks contents))
     ) `catchIOError` fileReadHandler
 fileToPackets filename "octet" = (do
     handle <- openBinaryFile filename ReadMode
-    contents <- Data.ByteString.hGetContents handle
+    contents <- BS.hGetContents handle
     return (mapWithIndex (\dat index -> DATA (fromIntegral (index+1)) dat) (chunks contents))
     ) `catchIOError` fileReadHandler
 fileToPackets filename mode = return [ERROR 0 ("Invalid mode " ++ mode)]
@@ -68,18 +96,18 @@ fileReadHandler e
     | otherwise = return [ERROR 2 ("Error: " ++ ioeGetErrorString e)]
 
 -- Split a bytestring into chunks of size blockSize
-chunks :: Data.ByteString.ByteString -> [Data.ByteString.ByteString]
-chunks b = if Data.ByteString.null b then [] else chunk:(chunks remainder)
-    where (chunk, remainder) = Data.ByteString.splitAt blockSize b
+chunks :: BS.ByteString -> [BS.ByteString]
+chunks b = if BS.null b then [] else chunk:(chunks remainder)
+    where (chunk, remainder) = BS.splitAt blockSize b
 
 -- Source: https://stackoverflow.com/a/16192050/2333689
 mapWithIndex :: (a -> Int -> b) -> [a] -> [b]
 mapWithIndex f l = zipWith f l [0..]
 
 
-fromByteString2 :: Data.ByteString.ByteString -> Maybe Packet
+fromByteString2 :: BS.ByteString -> Maybe Packet
 fromByteString2 d = fromCharArray2 opcode16 rest
-    where (opHigh:opLow:rest) = Data.ByteString.unpack d
+    where (opHigh:opLow:rest) = BS.unpack d
           opcode16 = word8to16 [opHigh,opLow]
 
 fromCharArray2 :: Word16 -> [Word8] -> Maybe Packet
@@ -96,18 +124,21 @@ fromCharArray2 5 (b1:b2:b) = Just (ERROR (word8to16 [b1,b2]) errMsg)
 fromCharArray2 x y = trace ("Received opcode=" ++ show x ++ " and data=" ++ (showList y "")) Nothing
 
 
-byteStringtoString :: Data.ByteString.ByteString -> String
-byteStringtoString = map (chr.fromEnum).Data.ByteString.unpack
+byteStringtoString :: BS.ByteString -> String
+byteStringtoString = map (chr.fromEnum).BS.unpack
+
+stringToByteString :: String -> BS.ByteString
+stringToByteString str = BS.pack (map (toEnum.ord) str)
 
 singleWord8to16 :: Word8 -> Word16
 singleWord8to16 = fromInteger.toInteger
 
 -- Convert the first two bytes of the given bytestring to a 2-byte integer
-byteStringToWord16 :: Data.ByteString.ByteString -> Word16
+byteStringToWord16 :: BS.ByteString -> Word16
 -- return ((higher << 8) | lower) :: Word16
 byteStringToWord16 bs = fromIntegral ((.|.) (high `shiftL` 8) (low))
-    where (high:low:_) = map fromEnum (Data.ByteString.unpack bs)
-        --   (high2:low2:_) = (Data.ByteString.unpack bs)
+    where (high:low:_) = map fromEnum (BS.unpack bs)
+        --   (high2:low2:_) = (BS.unpack bs)
         --   high = fromIntegral(fromEnum high2)
         --   low = fromIntegral(fromEnum low2)
 
@@ -121,4 +152,8 @@ extractString [] = ("", [])
 extractString (head:rest) = if head == 0 then ("", rest) else (show head ++ substring, remainder)
     where (substring, remainder) = extractString rest
 
-
+word16ToByteString :: Word16 -> BS.ByteString
+-- return [num >> 8, num & 255]
+word16ToByteString num = BS.pack [higher,lower]
+    where higher = fromInteger((toInteger num) `shiftR` 8)
+          lower = fromInteger(toInteger ((.&.) num 255))
--- src/UDP.hs
+++ src/UDP.hs
@@ -2,9 +2,11 @@ module UDP where
 
 import Control.Concurrent        (forkIO, threadDelay)
 import Control.Monad             (forever)
-import qualified Data.ByteString.Char8 as C
-import Network.Socket hiding     (recv)
-import Network.Socket.ByteString (recv, sendAll)
+-- import qualified Data.ByteString.Char8 as C
+import Network.Socket hiding     (recv, recvFrom, send, sendAll, sendAllTo)
+import Network.Socket.ByteString (recv, recvFrom, send, sendAll, sendAllTo)
+import Data.Maybe
+import qualified Data.ByteString (concat,length)
 import TFTP
 
 -- SOURCE: http://www.mchaver.com/posts/2017-06-12-haskell-network-programming-1.html
@@ -14,17 +16,22 @@ runUDPServer = do
   -- Resolve 127.0.0.1:7000. Returns array of possible values, best is first
   addrinfos <- getAddrInfo Nothing (Just "127.0.0.1") (Just "7000")
   let serveraddr = head addrinfos
-  sock <- socket (addrFamily serveraddr) Datagram defaultProtocol
+  listenSocket <- socket (addrFamily serveraddr) Datagram defaultProtocol
   -- Bind socket (ie. start listening)
-  bind sock (addrAddress serveraddr)
-  portNum <- socketPort sock
+  bind listenSocket (addrAddress serveraddr)
+  portNum <- socketPort listenSocket
   putStrLn ("Started server on port " ++ (show portNum))
   forever (do
-    socketData <- recv sock 4096
+    (socketData, recvAddr) <- recvFrom listenSocket 4096
     putStrLn("<<<< Received data: " ++ show socketData)
-    -- let tftpPacket = fromByteString2 socketData
     let tftpPacket = fromByteString socketData
-    putStrLn("<<<< Packet: " ++ show tftpPacket))
-    -- sendAll sock socketData
-    -- putStrLn(">>>> Replied data: " ++ show socketData))
+    -- let tftpPacket = fromByteString2 socketData
+    putStrLn("<<<< Packet: " ++ show tftpPacket)
+    if (isNothing tftpPacket) then return () else (do
+      responsePackets <- process (fromJust tftpPacket)
+      putStrLn(">>>> Packets: " ++ showList responsePackets "")
+      let response = Data.ByteString.concat (map toByteString responsePackets)
+      -- putStrLn(">>>> Response data: " ++ byteStringtoString response)
+      replySocket <- socket (addrFamily serveraddr) Datagram defaultProtocol
+      sendAllTo replySocket response recvAddr))
 
