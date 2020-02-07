9b36bfccb5be6706f8b00fdb104dd2ac79ca7779
Juan Li Puma <juanlipuma94@gmail.com>
2020-02-06 22:13:12 -0300

    Reorder functions, indent code

--- src/Netascii.hs
+++ src/Netascii.hs
@@ -24,25 +24,34 @@ isNetascii str = BS.all isInRange str
 
 --- PRIVATE
 
-encodeLineEndings :: BS.ByteString -> BS.ByteString
-encodeLineEndings line = if BS.null line then line else BS.concat [processedPrefix, processedSuffix] where
-    (prefix, suffix) = BS.break isLineBreak line
-    (processedPrefix, charsConsumed) = encodeChunk prefix suffix
-    processedSuffix = if BS.null suffix then suffix else encodeLineEndings safeSuffix
-    safeSuffix = BS.drop charsConsumed suffix
-
 isInRange :: Word8 -> Bool
 isInRange char = minValue <= char && char <= maxValue
 
 isLineBreak :: Word8 -> Bool
 isLineBreak x = x == LF || x == CR -- TODO reduce
 
+encodeLineEndings :: BS.ByteString -> BS.ByteString
+encodeLineEndings line = if BS.null line
+    then line
+    else BS.concat [processedPrefix, processedSuffix] where
+        (prefix, suffix) = BS.break isLineBreak line
+        (processedPrefix, charsConsumed) = encodeChunk prefix suffix
+        processedSuffix = if BS.null suffix
+            then suffix
+            else encodeLineEndings safeSuffix
+        safeSuffix = BS.drop charsConsumed suffix
+
+-- Encode a chunk boundary, also returning how many characters from the suffix were consumed
 encodeChunk :: BS.ByteString -> BS.ByteString -> (BS.ByteString, Int)
-encodeChunk prefix suffix = if BS.null suffix then (prefix, 0) else (BS.concat[prefix, processedSuffix], charsConsumed) where
-    char1 = BS.head suffix
-    char2 = if BS.length suffix < 2 then Nothing else Just(BS.head (BS.tail suffix))
-    (rawProcessedSuffix, charsConsumed) = encodePair char1 char2
-    processedSuffix = BS.pack rawProcessedSuffix
+encodeChunk prefix suffix = if BS.null suffix
+    then (prefix, 0)
+    else (BS.concat[prefix, processedSuffix], charsConsumed) where
+        char1 = BS.head suffix
+        char2 = if BS.length suffix < 2
+            then Nothing
+            else Just $ BS.head $ BS.tail suffix
+        (rawProcessedSuffix, charsConsumed) = encodePair char1 char2
+        processedSuffix = BS.pack rawProcessedSuffix
 
 -- Insert CR/LF as necessary and inform how many characters of the original string were consumed
 encodePair :: Word8 -> Maybe Word8 -> ([Word8], Int)
