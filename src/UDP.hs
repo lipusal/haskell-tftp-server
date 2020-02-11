03db85df28919fd2a06d31cb58458f65bbdd8a49
Juan Li Puma <juanlipuma94@gmail.com>
2020-02-11 00:16:37 -0300

    Boom, zero-effort concurrency

--- src/UDP.hs
+++ src/UDP.hs
@@ -4,6 +4,7 @@ import Control.Concurrent        (forkIO, threadDelay)
 import Control.Monad             (forever)
 import Network.Socket hiding     (recv, recvFrom)
 import Network.Socket.ByteString (recv, recvFrom)
+import qualified Data.ByteString (ByteString)
 import qualified TFTP
 
 -- SOURCE: http://www.mchaver.com/posts/2017-06-12-haskell-network-programming-1.html
@@ -27,7 +28,7 @@ runUDPServer portNum = do
   masterSocket <- openPort $ Just $ show portNum
   actualPortNum <- socketPort masterSocket
   putStrLn $ "Started server on port " ++ show actualPortNum
-  forever (mainLoop masterSocket)
+  forever $ mainLoop masterSocket
 
 mainLoop :: Socket -> IO ()
 mainLoop listenSock = do
@@ -36,6 +37,12 @@ mainLoop listenSock = do
   connect socket txSocketAddr -- Connect to client on other end
   portNum <- socketPort socket
   putStrLn("Established socket pair <127.0.0.1:" ++ show portNum ++ ", " ++ show txSocketAddr ++ "> for new connection")
+  forkIO $ handle portNum txSocketAddr rxData socket
+  -- handle portNum txSocketAddr rxData socket
+  return ()
+
+handle :: PortNumber -> SockAddr -> Data.ByteString.ByteString -> Socket -> IO ()
+handle portNum txSocketAddr rxData socket = do
   TFTP.handle rxData socket
   putStrLn("Connection <127.0.0.1:" ++ show portNum ++ ", " ++ show txSocketAddr ++ "> complete, closing socket")
   close' socket -- close' to throw exception if underlying system throws exception
